be6c1ab2fd5185e9945b4919b5d4fd88794527b8
razorboy73 <joshadamkerbel@gmail.com>
2024-06-24 07:19:14 -0400

    added print statements and formatting'

--- network_scanner.py
+++ network_scanner.py
@@ -10,30 +10,35 @@ Parse response
 Print response
 """
 
+
 def main():
     address_output(scan("192.168.0.1/24"))
+
+
 def scan(ip):
     # create an arp request directed to broadcast MAC asking for IP
     # Part 1 - ask who has the target IP
     arp_request = ARP(pdst=ip)
-    arp_request.show()
+    #arp_request.show()
     # print(arp_request.summary())
     # Part 2 - set destination MAC to Broadcast MAC; involves combining frames to broadcast
     # packets.  Involes creating a frame
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
+    # broadcast.show()
     scapy.ls(scapy.Ether())
     # print(broadcast.summary())
     # create the frame
-    arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    arp_request_broadcast = broadcast / arp_request
+    # print(arp_request_broadcast.summary())
+    #arp_request_broadcast.show()
     # send packet and capture response
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     return answered_list
 
+
 def address_output(answered_list):
-    print(answered_list)
+    #parses response and prints it
+    # print(answered_list)
     # print(unanswered_list)
     # print(f"answered: {answered_list.summary()}")
     # print(f"unanswered: {unanswered_list.summary()}")
@@ -54,14 +59,9 @@ def address_output(answered_list):
     # pdst = 192.168.0.32
     # ###[ Padding ]###
     # load = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    print("IP\t\t\tMAC Address\n------------------------------------")
     for element in answered_list:
-        print(f"MAC address: {element[1].hwsrc}")
-        print(f"IP Address: {element[1].psrc}")
-        print("****************************************************")
-
-
-
-
+        print(f"{element[1].psrc}\t\t{element[1].hwsrc}")
 
 
 if __name__ == "__main__":
