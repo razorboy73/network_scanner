6f604a2a3f9264e63516658cb920cdd41c4e310d
root <root@kali.kali>
2024-12-22 13:13:57 -0600

    accepts command line arguments

--- network_scannerv2.py
+++ network_scannerv2.py
@@ -9,7 +9,22 @@ Send packets and receive response
 Parse response
 Print response
 """
+def main():
+    ip_range = get_command_line_args()
+    address_output(scan(ip_range))
+    
+def get_command_line_args():
+    parser = argparse.ArgumentParser(description='Input the ip ranges you want to scan in CIDR notation')
+    parser.add_argument("-ip", "--ip_range", dest="ip", type=str, help='The ip range you want to scan in CIDR')
 
+    args = parser.parse_args()
+    print(f'IP Ranges: {args.ip}')
+    if not args.ip:
+        parser.error("[-] Please specify an IP range --help for assistance")
+    # handled errors for the mac address within the arguments
+    return args.ip    
+    
+    
 
 def scan(ip):
     #set the IPfield to the ip address of interest
@@ -28,14 +43,28 @@ def scan(ip):
     answered_list = scapy.srp(arp_request_broadcast, timeout = 2)[0]
     print(answered_list.summary())
     #parsse out the hardward and digital sources
-    
+    clients_lists = []
     print("IP\t\t\tMAC Address\n-------------------------------------------------")
     for element in answered_list:
+        #create a dictionary
+        client_dict = {"ip":element[1].psrc, "mac_address": element[1].hwsrc }
+        #apennd dictionary to list
+        clients_lists.append(client_dict)
         print(element[1].psrc +"\t\t"+ element[1].hwsrc)
+
         
         print("-------------------------------------------------")
+    return (clients_lists)
+
+def address_output(clients_lists):
+    print("IP\t\t\tMAC Address\n-------------------------------------------------")
+    for client in clients_lists:
+        print(f"{client['ip']}\t\t{client['mac_address']}")
+    print("------------------------------------------------")
+        
     
+if __name__ == "__main__":
+    main()
 
     
     
-scan("172.16.149.163")
